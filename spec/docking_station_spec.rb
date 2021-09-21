ac0dbb9860ad5d7a8547d55d1984da8b85d63bf3
charlie83xt <charlie83xt@hotmail.com>
2021-09-21 17:09:16 +0100

    Macking docking stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,18 @@
 require "docking_station"
 
 describe DockingStation do
-    it "should release a bike when requested" do 
+    it "it should respond to release a bike" do 
      docking_station = DockingStation.new
      expect(docking_station).to respond_to(:release_bike)
     end
+    it "it should get a bike" do 
+        bike = Bike.new
+        docking_station = DockingStation.new
+        expect(docking_station.release_bike).to be_a Bike 
+        expect(bike.working?).to eq(true)
+    end
+    # it "bike should be working?" do 
+    #     docking_station = DockingStation.new
+    #     expect(docking_station.release_bike()).(:release_bike)
+    #    end
 end
