c86c150b520ac500d88ce9a9a43021e43eca6ba0
charlie83xt <charlie83xt@hotmail.com>
2021-09-21 15:40:32 +0100

    Creating docking station class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require "docking_station"
 
 describe DockingStation do 
-    it "should release a bike when requested" do 
-        release = DockingStation.new
-        expect(release.check("Release")).to(eq(true))
-    end
+    # it "should release a bike when requested" do 
+    #     release = DockingStation.new
+    #     expect(release.check("Release")).to(eq(true))
+    # end
 end
