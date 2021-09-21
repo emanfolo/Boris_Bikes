5b3325f426119763d3bbbed68444efcf5f0c3af3
Emmanuel Folorunsho <emmanuel@Emmanuels-MacBook-Air.local>
2021-09-21 16:16:50 +0100

    Creating bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require "docking_station"
 
-describe DockingStation do 
-    # it "should release a bike when requested" do 
-    #     release = DockingStation.new
-    #     expect(release.check("Release")).to(eq(true))
-    # end
+describe DockingStation do
+    it "should release a bike when requested" do 
+     docking_station = DockingStation.new
+     expect(docking_station).to respond_to(:release_bike)
+    end
 end
