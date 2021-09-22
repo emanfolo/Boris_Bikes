59e93a18fc5eab847fa00ffe5e014bf0aa342d39
Emmanuel Folorunsho <emmanuel@Emmanuels-MacBook-Air.local>
2021-09-22 14:42:53 +0100

    Adding a counter and docking feature to the docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,22 @@
 require "bike"
 
 class DockingStation
+    attr_reader :counter  
+
+    def initialize
+      @counter = 0
+    end
+
     def release_bike
       Bike.new
+      #@counter -= 1
+    end 
+
+    def dock 
+      Bike.new
+    end 
+
+    def add_counter
+      @counter += 1 
     end 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,8 +11,17 @@ describe DockingStation do
         expect(docking_station.release_bike).to be_a Bike 
         expect(bike.working?).to eq(true)
     end
-    # it "bike should be working?" do 
-    #     docking_station = DockingStation.new
-    #     expect(docking_station.release_bike()).(:release_bike)
-    #    end
+    it "it should add a bike to counter for our docking station" do
+        #Arrange
+        bike = Bike.new
+        docking_station = DockingStation.new
+        #Act
+        docking_station.add_counter
+        expect(docking_station.counter).to eq(1)
+    end
+    it "it should create an instance of the Bike class" do 
+        bike = Bike.new
+        docking_station = DockingStation.new
+        expect(docking_station.dock).to be_a Bike
+    end
 end
