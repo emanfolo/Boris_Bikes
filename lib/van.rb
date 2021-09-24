3e1919d0b22223cc8b6336df0b55f42380647184
Tom <t.p.kellett@gmail.com>
2021-09-24 16:20:38 +0100

    Adding further tests and feature to collect broken bikes from docking stations
    Co-authored-by: Emmanuel <emmanuelfolorunsho@icloud.com>

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,15 @@
 class Van
+  attr_accessor :collected_bikes
+
+  def initialize
+    @collected_bikes = []
+  end
+
   def collect_bikes(docking_station)
-    raise "There are no bikes" unless !docking_station.bikes.empty?
-    []
+    raise "There are no bikes" unless (!docking_station.bikes.empty?)
+    until docking_station.bikes[0].working? do
+        @collected_bikes << docking_station.bikes.shift
+    end
+    @collected_bikes
   end
 end 
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,19 +1,34 @@
 require "van"
+require "bike"
 
 describe Van do 
-  let (:london_bridge) {double("london_bridge", :bikes => ["2","3"])}
+  let (:bike) {double("bike", :is_working => true)}
+  let (:broken_bike) {double("broken_bike", :is_working => false)}
+
+  let (:london_bridge) {double("london_bridge", :bikes => [bike, bike])}
   let (:empty_station) {double("empty_station", :bikes => [])}
+  let (:liverpool_street) {double("liverpool_street", :bikes => [broken_bike, broken_bike, bike, bike])}
   
+  before do 
+    allow(bike).to receive(:working?).and_return(true)
+    allow(broken_bike).to receive(:working?).and_return(false)
+  end
+
   it "Should be respond to collect bikes with an argument of docking station" do 
     expect(subject).to respond_to(:collect_bikes).with(1).argument
 
   end
   it "Should be able to collect an array of bikes from the docking station" do 
-    expect(subject.collect_bikes(london_bridge)).to be_a Array
+    expect(subject.collect_bikes(liverpool_street)).to be_a Array
   end
 
   it "Should raise an error when collect bikes is called on an empty array" do 
     expect{subject.collect_bikes(empty_station)}. to raise_error("There are no bikes")
+  end
+
+  it "Should only take broken bikes from the docking station" do
+    subject.collect_bikes(liverpool_street)
+    expect(subject.collected_bikes.all? {|bikes| !bikes.working?}).to eq true
   end
 
 
